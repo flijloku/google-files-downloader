2d076de6d3c8effb3142747a2a91189a03848d76
flijloku <flijloku@yahoo.com>
2025-07-26 02:12:51 +0300

    Update googleFilesDownloader.sh

--- googleFilesDownloader.sh
+++ googleFilesDownloader.sh
@@ -126,9 +126,9 @@ elif [ "$is_torrent" == "1" ]; then
 	if [ "${FULL^^}" == "N" ]; then
 		aria2c -S "$TORRENT_FILE";
 		read -p "Please select a files (1,2,3): " FILES;
-		aria2c --allow-overwrite --disable-ipv6 --seed-time=0 --seed-ratio=0.0 --select-file="$FILES" "$TORRENT_FILE";
+		aria2c --allow-overwrite --disable-ipv6 --enable-dht --seed-time=0 --seed-ratio=0.0 --select-file="$FILES" "$TORRENT_FILE";
 	else
-		aria2c --allow-overwrite --disable-ipv6 --seed-time=0 --seed-ratio=0.0 "$TORRENT_FILE";
+		aria2c --allow-overwrite --disable-ipv6 --enable-dht --seed-time=0 --seed-ratio=0.0 "$TORRENT_FILE";
 	fi
 elif [ "$is_magnet" == "1" ]; then
 	hash=$(echo "$URL" | grep -oP "(?<=btih:).*?(?=&)");
@@ -142,9 +142,9 @@ elif [ "$is_magnet" == "1" ]; then
 		aria2c --bt-metadata-only=true --bt-save-metadata=true -q "$URL";
 		aria2c -S "$hash.torrent";
 		read -p "Please select a files (1,2,3): " FILES;
-		aria2c --allow-overwrite --disable-ipv6 --seed-time=0 --seed-ratio=0.0 --select-file="$FILES" $hash.torrent;
+		aria2c --allow-overwrite --disable-ipv6 --enable-dht --seed-time=0 --seed-ratio=0.0 --select-file="$FILES" $hash.torrent;
 	else
-		aria2c --allow-overwrite --disable-ipv6 --seed-time=0 --seed-ratio=0.0 "$URL";
+		aria2c --allow-overwrite --disable-ipv6 --enable-dht --seed-time=0 --seed-ratio=0.0 "$URL";
 	fi
 fi
 if [ -e "$hash.torrent" ]; then
